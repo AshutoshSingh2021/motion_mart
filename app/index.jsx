ea4a8b8a7f9857cdb8ff1e3a22a4a885eb5771f8
AshutoshSingh2021 <ashutoshsingh15102@gmail.com>
2024-07-06 20:06:47 +0530

    Initial Commit

---
+++ app/index.jsx
@@ -0,0 +1,50 @@
+import { Image, ScrollView, Text, View } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { Link } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { images } from "../constants";
+import CustomButton from "../components/CustomButton";
+
+const App = () => {
+  return (
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView contentContainerStyle={{ height: "100%" }}>
+        <View className="w-full h-full items-center px-4">
+          <Image
+            source={images.logo}
+            className="w-[140px] h-[90px]"
+            resizeMode="contain"
+          />
+          <Image
+            source={images.cards}
+            className="max-w-[380px] w-full h-[300px]"
+            resizeMode="contain"
+          />
+          <View className="relative mt-5">
+            <Text className="text-2xl text-white font-bold text-center">
+              Discover Endless Possibilities with{" "}
+              <Text className="text-secondary-200">Motion Mart</Text>
+            </Text>
+            <Image
+              source={images.path}
+              className="w-[335px] h-[15px] absolute -bottom-2 -right-12"
+              resizeMode="contain"
+            />
+          </View>
+          <Text className="text-sm font-pregular mt-7 text-gray-100 text-center">
+            Where creativity meets innovation: embark on a journey of limitless
+            exploration with Motion Mart
+          </Text>
+          <CustomButton
+            title="Continue with Email"
+            handlePress={() => {}}
+            containerStyles="w-full mt-7"
+          />
+        </View>
+      </ScrollView>
+      <StatusBar backgroundColor="#161622" style="light" />
+    </SafeAreaView>
+  );
+};
+
+export default App;
